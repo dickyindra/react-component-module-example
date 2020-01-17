c0ea82a20c49de5a61e741395d75e2ba48fe395f
dickyindra <dickyindraofficial@gmail.com>
2020-01-17 13:37:01 +0700

    fix structure sources

---
+++ src/components/example/example.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Example = () => <span>Example...</span>
+
+export default Example
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const ExampleComponent = () => <span>Example...</span>
-
-export default ExampleComponent
