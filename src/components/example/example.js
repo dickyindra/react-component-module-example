8626f2a9616faa4c585da4ddc7b66f590706bc78
dickyindra <dickyindraofficial@gmail.com>
2020-01-17 14:32:57 +0700

    fix example component design

--- src/components/example/example.js
+++ src/components/example/example.js
@@ -1,5 +1,13 @@
 import React from "react"
 
-const Example = () => <div className='example'>Example...</div>
+const Example = () => (
+    <div className='example'>
+        <img src='https://i.pravatar.cc/300' className='example-avatar' />
+        <div className='example-detail'>
+            <span className='example-detail-name'>Dicky Indra Asmara</span>
+            <span className='example-detail-message'>Assalamu'alaikum Bapak Ibu sekalian..</span>
+        </div>
+    </div>
+)
 
 export default Example
